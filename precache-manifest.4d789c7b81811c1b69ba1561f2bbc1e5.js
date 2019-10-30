bd7f9db0c6e3b90fc9f49f201fbf0ea985dbcff6
Manigandan Saravanan <mg@Manigandans-MacBook-Pro.local>
2019-10-30 23:34:05 +0530

    Updates

---
+++ precache-manifest.4d789c7b81811c1b69ba1561f2bbc1e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b463d3a4356ef14aa4c4f435c644f38c",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "93802b57a93e5bd20932",
+    "url": "/cats-rolodex/static/css/main.8d6f615d.chunk.css"
+  },
+  {
+    "revision": "fc9e4de217e42ee0a327",
+    "url": "/cats-rolodex/static/js/2.23f7dbf8.chunk.js"
+  },
+  {
+    "revision": "93802b57a93e5bd20932",
+    "url": "/cats-rolodex/static/js/main.54a550a7.chunk.js"
+  },
+  {
+    "revision": "51ddaa90d56d8a717306",
+    "url": "/cats-rolodex/static/js/runtime-main.6f0b3655.js"
+  }
+]);
